cb496a2e6b5269d2305e546e528a01d4d9c1bb2a
alfonsopintos <alfonsopintos@gmail.com>
2015-11-12 16:42:51 -0500

    modified contact email address

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,8 +8,8 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
-      :to => "your_email@example.org",
+      :subject => "Contact Inquiry",
+      :to => "alfonsopintos@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
   end
